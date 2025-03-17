e8823bc7c2ed85213510ae965b32a4da89f3318e
evgenii <vovk.evgenii.199811@gmail.com>
2025-03-17 14:19:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Trip from './pages/Trip'
+
+function App() {
+  return (
+    <>
+      <Trip />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Trip.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function Trip() {
+  return (
+    <div className={styles["trip"]}>
+      <h2 className={styles["title"]}>
+        Поездки
+      </h2>
+    </div>
+  )
+};

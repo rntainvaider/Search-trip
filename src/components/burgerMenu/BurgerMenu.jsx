31c58c7c104bd39177d03896ae7e85c0d46f9b24
evgenii <vovk.evgenii.199811@gmail.com>
2025-03-17 14:34:27 +0300

    работа над header страницы trip

---
+++ src/components/burgerMenu/BurgerMenu.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function BurgerMenu() {
+  return (
+    <div className={styles["burger"]}>
+      <div className={styles["line"]}>
+
+      </div>
+    </div>
+  )
+};
--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -1,11 +1,15 @@
+import BurgerMenu from "../components/burgerMenu/BurgerMenu";
 import styles from "./styles.module.css";
 
 export default function Trip() {
   return (
     <div className={styles["trip"]}>
-      <h2 className={styles["title"]}>
-        Поездки
-      </h2>
+      <header className={styles["header"]}>
+        <h2 className={styles["header__title"]}>
+          Поездки
+        </h2>
+        <BurgerMenu />
+      </header>
     </div>
   )
 };
